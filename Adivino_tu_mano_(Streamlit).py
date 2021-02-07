0f8d35ed33af55b74a7cc9ac393d838c64bd707d
Alexander Ortega <65872775+alexandergribenchenko@users.noreply.github.com>
2021-02-07 17:19:32 -0500

    Actualización del modelo, de la librería PIL y de la posibilidad del resize

--- Adivino_tu_mano_(Streamlit).py
+++ Adivino_tu_mano_(Streamlit).py
@@ -7,17 +7,20 @@ Created on Sat Feb 06 2021
 import numpy as np
 import streamlit as st
 import tensorflow as tf
-import imageio
+from PIL import Image, ImageOps
 
-def import_and_predict(image, model): 
-        image_paso_01 = image[:,:,0]
+def transform_and_predict(image, model):
+        size = (64,64)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+        image_paso_00 = np.asarray(image)
+        image_paso_01 = image_paso_00[:,:,0]
         image_paso_02 = np.array([image_paso_01])
         image_paso_03= image_paso_02.reshape(image_paso_02.shape[0], image_paso_02.shape[1], image_paso_02.shape[2], 1)
         image_paso_04 = image_paso_03.astype('float32')/255
         prediction = model.predict(image_paso_04)
         return prediction
 
-model = tf.keras.models.load_model('modelo_01.hdf5')
+model = tf.keras.models.load_model(Adivino_tu_mano_(Modelo).hdf5')
 
 st.write("""
          # !Adivino tu mano!
@@ -32,9 +35,9 @@ file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
 if file is None:
     st.text("Aún no has subido ninguna imagen. !Anímate a hacerlo!")
 else:
-    image = np.asarray(imageio.imread(file))
+    image = Image.open(file)
     st.image(image, use_column_width=True)
-    prediction = import_and_predict(image, model)
+    prediction = transform_and_predict(image, model)
     
     if np.argmax(prediction) == 0:
         st.write("!Yo que te digo que eso es un cero!")
